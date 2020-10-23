69458798c0ba241d236f5f0588a7c172076393b7
laser-xs <47507083+laser-xs@users.noreply.github.com>
2020-10-24 08:40:54 +1100

    Create url_status.py

---
+++ url_status.py
@@ -0,0 +1,28 @@
+import requests, sys
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from socket import gethostbyname, gaierror
+
+if len(sys.argv) < 2:
+    print("\nUsage: " + sys.argv[0] + " <file containing URLs>\n")
+    sys.exit()
+
+#Returns the HTTP status of the URL
+def main():
+    with open(url_list, 'r') as f:
+        print("Checking URL status...\n")
+        errors = []
+        for url in f.readlines(): 
+            url=url.rstrip('\n')
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+            try:
+                r = requests.head(url, verify=False)
+                print ("[+]",url,"-", r.status_code)
+            except (OSError, requests.ConnectionError, requests.exceptions.HTTPError) as err:
+                err = "- Hostname Error"
+               #print("[-]",url,err)
+                errors.append(url) 
+        print("")
+        print("[!] The following hostname(s) may be invalid:", *errors,sep='\n')
+        print("")
+url_list = sys.argv[1]
+main()
