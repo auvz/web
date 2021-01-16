749eef63ce6f8cd2c7d9493d6110e5a1170f1afd
laser-xs <47507083+laser-xs@users.noreply.github.com>
2021-01-16 18:02:11 +1100

    Create vpn_routes.sh

---
+++ vpn_routes.sh
@@ -0,0 +1,28 @@
+#function to resolve hostnames and store variable in array
+resolve_hostname() {
+        for host in "${varhost[@]}";
+        do
+                ip_arr+=("`host $host | awk '/has address/ { print $4 }'`")
+        done
+echo 'Routes for the following IP will be added:'
+printf '%s\n' "${ip_arr[@]}"
+}
+
+#TODO: fix array parsing
+
+#calls the resolve_hostname function and adds routes to the VPN gateway variable
+add_routes() {
+        resolve_hostname
+        for ip in ${ip_arr[@]};
+        do
+        sudo route add $ip -netmask 255.255.255.255 $vpn
+        sleep 1
+        done
+}
+
+echo Enter hostname:
+read -a varhost
+echo Enter vpn IP gateway:
+read vpn
+declare -a ip_arr
+add_routes
