3473d17162417aebedb26254e4b530af06fefcdc
laser-xs <47507083+laser-xs@users.noreply.github.com>
2021-01-27 11:31:37 +1100

    Update routes.sh

--- routes.sh
+++ routes.sh
@@ -1,42 +1,49 @@
 #function to resolve hostnames and store variable in array
 resolve_hostname() {
-	for host in "${varhost[@]}";
-	do 
-		ip_arr+=("`host $host | awk '/has address/ { print $4 }'`")
-	done
+        for host in "${varhost[@]}";
+        do      
+                ip_arr+=("`host $host | awk '/has address/ { print $4 }'`")
+                if [[ -n $host ]]; then
+                        ip_resolve+=($ip)
+                fi
+        done
 
-# Use this notation
+# Use this notation for resolving index differenece between bash and zsh
 # ${array[@]:offset:length}
 # array[@] is all the elements, 0 is the offset (always is 0-based), and 1 is the number of elements desired.
+# if [[ ${ip_arr[@]:0:1} == "" ]]; then
 
-if [[ ${ip_arr[@]:0:1} == "" ]]; then 
+# TODO - could improve logic by showing which hosts were unable to be resolved
+#printf '%s\n' "${ip_fail[@]}"
 
-# TODO - logic is stuffed, if one host resolves and one doesn't then it'll still return the below error
-		printf '[!] Unable to resolve hostnames'
-	else
-		echo '[+] A route for the following will be added:'
-	fi
-printf '%s\n' "${ip_arr[@]}"
+        if [ ${#ip_resolve[@]} -eq  0 ]; then
+        printf '[!] Unable to resolve hostnames'
+        else    
+                echo '[+] A route for the following will be added:'
+        fi
+printf '%s\n' "${ip_resolve[@]}"
 }
 
-# calls the resolve_hostname function and adds routes to the hardcoded VPN gateway i.e. 10.1.1.1
+# calls the resolve_hostname function and adds routes to the hardcoded VPN gateway i.e. 10.1.1.1, could make this variable but unlikely to change
 # if you want to preserve spaces, use quotation marks around the array, but this screws up the script
 
 add_routes() {
-	resolve_hostname
-	for ip in ${ip_arr[@]};
-	do
-		sudo route add $ip -netmask 255.255.255.255 10.1.1.1 > /dev/null
+        resolve_hostname
+        for ip in ${ip_arr[@]};
+        do
+                sudo route add $ip -netmask 255.255.255.255 10.1.1.1 2> /dev/null
 # decide whether to redirect STDOUT and STDERR to /dev/null (&>)
 # just STDERR (2>)
-		sleep 0.5
-	done
+                sleep 0.5
+        done
 }
 
-# -p   Display the attributes and values of each name. When '-p' is used, additional options are ignored.
 
-echo '[*] Enter Hostnames (seperated by a space):'
+# -p   For an array, display the attributes and values of each name. When '-p' is used, additional options are ignored.
+
+echo '[*] Enter Hostnames (Seperated by a space):'
 read -a varhost
 echo ''
 declare -a ip_arr
+declare -a ip_resolve
 add_routes
