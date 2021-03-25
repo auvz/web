029ed1c2c2f9def33f027a6befc80a59dc1d0996
laser-xs <47507083+laser-xs@users.noreply.github.com>
2021-03-25 15:14:58 +1100

    Update passwd.py

--- passwd.py
+++ passwd.py
@@ -4,12 +4,6 @@ chars = digits + ascii_lowercase + ascii_uppercase
 # special_chars = "\"!@#$^&*()-+_,"
 # passwd = chars + special_chars
 
-L1 = ["".join([choice(chars) for i in range(6)]) for j in range(1)]
-L2 = ["".join([choice(chars) for i in range(6)]) for j in range(1)]
-L3 = ["".join([choice(chars) for i in range(6)]) for j in range(1)]
+L1 = ["".join([choice(chars) for i in range(6)]) for j in range(3)]
 
-# output password
-
-L = L1 + L2 + L3
-
-print(*L,sep='-')
+print(*L1,sep='-')
